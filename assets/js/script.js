c4091365e13528a7eee48cbf5e31b70916c8eee2
Michael Mosquera <michael2000ny@gmail.com>
2024-10-10 15:53:42 -0400

    created backBtn

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@ function showPersonality() {
     document.getElementById('animals').classList.remove('active');
     document.getElementById('personality').classList.add('active');
 }
+
+function showAnimals() {
+    document.getElementById('personality').classList.remove('active');
+    document.getElementById('animals').classList.add('active');
+}
