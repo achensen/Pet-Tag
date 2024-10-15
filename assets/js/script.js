a91740ea76a93fc74cd6f2d4ddd2fc6f33b823b0
Michael Mosquera <michael2000ny@gmail.com>
2024-10-14 21:55:17 -0400

    made keep name btn save to storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,16 @@ function getName() {
     const names = ['Charlie', 'Michael', 'Khadijih', 'Erin', 'Mati', 'Zippy', 'Dash', 'Snuggles', 'Shadow', 'Snugs', 'Dozer', 'Echo', 'Gizmo', 'Nibbles', 'Breeze', 'Sonny', 'Puff', ];
     const randomName = names[Math.floor(Math.random() * names.length)];
     
+    storedName = randomName;
+
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = `Your pet's new name is ${randomName}!`;    
     resultDiv.style.display = 'block';
     document.getElementById('keepNameBtn').classList.add('active');
-
-    localStorage.setItem('savedPetName', randomName);
 }
 
-function displayKeptName() {
-    const savedName = localStorage.getItem('savedPetName');
-    alert(`${savedName} has been saved!`);
-}
+document.getElementById('keepNameBtn').addEventListener('click', function() {
+    alert(`You have kept the name: ${storedName}`);
+    localStorage.setItem('savedPetName', storedName);
+
+});
